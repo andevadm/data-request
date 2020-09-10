b86b995989a7867d1d84c6270b2b3562d8c16062
Development Admin <an.devadm@gmail.com>
2020-09-10 12:44:50 +0300

    Initial configuration

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        The app is under development
+      </div>
+    </div>
+  );
+}
+
+export default App;
