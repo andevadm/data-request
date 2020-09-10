d3d61a6dab48e566b90e32be3d48f5ffb6a3d5c4
Development Admin <an.devadm@gmail.com>
2020-09-10 12:51:25 +0300

    Updates

---
+++ precache-manifest.33ce7f759d7e996ae7f7dd189147accd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad263c753829e8894b003b92c4b8951",
+    "url": "/data-request/index.html"
+  },
+  {
+    "revision": "5bf9fc362edb24d22f3b",
+    "url": "/data-request/static/css/main.ad0c4a64.chunk.css"
+  },
+  {
+    "revision": "a4e0e66b6235ef29da61",
+    "url": "/data-request/static/js/2.3a8677aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/data-request/static/js/2.3a8677aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf9fc362edb24d22f3b",
+    "url": "/data-request/static/js/main.9d5b61b2.chunk.js"
+  },
+  {
+    "revision": "cda1c8a116562605dce1",
+    "url": "/data-request/static/js/runtime-main.13b8d735.js"
+  }
+]);
